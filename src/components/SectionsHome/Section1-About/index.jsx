a05178d6627e4f41ecec6b7d65c1a9ba3a92fb30
guicamargo <gcamargo303@gmail.com>
2025-01-24 22:10:21 -0300

    formatacao sessao about

--- src/components/SectionsHome/Section1-About/index.jsx
+++ src/components/SectionsHome/Section1-About/index.jsx
@@ -4,15 +4,31 @@ import './styles.css';
 
 import ConteinerSections from "../../conteinerSections";
 
+import ImgPerfil from '../../../images/Perfil/IMG_Perfil.JPG';
+
 const Index = () => {
   const { t } = useTranslation();
   return (
     <ConteinerSections title={t('nav.about')}>
       <div className="aboutContainer">
       <div className="imageContainer">
+      <img src={ImgPerfil} alt="Profile" className="profileImage" />
+      </div>
+      <div className='name'>
+      <div className='nameContainer'>
+        Guilherme
+      </div>
+      <div className='lastNameContainer'>
+        Correa Camargo
+      </div>
       </div>
       <div className="infoContainer">
-        About
+        <p className="infoText">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Perferendis perspiciatis corporis doloremque, reiciendis tempore iste sapiente sed voluptates minima hic.
+          Quasi aut illo iusto vitae nostrum. Vitae eveniet quidem quia.        
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
         </div>
 
       </div>
