52000baa92917e512bcf3bd9f3a79bf4b41cfa2b
guicamargo <gcamargo303@gmail.com>
2025-01-29 10:00:24 -0300

    config vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg'], // Add all necessary image file types
-  base: '/portfolio-frontend/'
+  base: './', // Mudando para caminho relativo para funcionar no Vercel
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
+  assetsInclude: ['**/*.JPG', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg']
 })
